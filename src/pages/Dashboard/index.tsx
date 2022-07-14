fcfc2b18d6c6179cf248047f31b1cf59b2cf5c60
Andre De Carolis <decarolis.dev@gmail.com>
2022-07-14 16:26:02 +0100

    Estilização do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
 // styled-components
-import { Title, Form } from './styles';
+import { Title, Form, Repos } from './styles';
 
 export const Dashboard: React.FC = () => {
   return (
@@ -13,6 +14,20 @@ export const Dashboard: React.FC = () => {
         <input type="text" placeholder="username/repository_name" />
         <button type="submit">Buscar</button>
       </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/68967867?v=4"
+            alt="Repositório"
+          />
+          <div>
+            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
+            <p>Repositório do mini curso gratuito de reactjs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
     </>
   );
 };
