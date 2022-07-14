572e93bdaaa1776c75c763343fe74051d9d76c38
Andre De Carolis <decarolis.dev@gmail.com>
2022-07-14 19:43:05 +0100

    Persistência em localStorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,31 +2,87 @@ import React from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
+// api
+import { api } from '../../services/api';
+
 // styled-components
-import { Title, Form, Repos } from './styles';
+import { Title, Form, Repos, Error } from './styles';
+
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 export const Dashboard: React.FC = () => {
+  const [repos, setRepos] = React.useState<GithubRepository[]>(() => {
+    const storageRepos = localStorage.getItem('@GitCollection:repositories');
+    if (storageRepos) {
+      return JSON.parse(storageRepos);
+    }
+    return [];
+  });
+  const [newRepo, setNewRepo] = React.useState('');
+  const [inputError, setInputError] = React.useState('');
+
+  React.useEffect(() => {
+    localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+  }, [repos]);
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setNewRepo(event.target.value);
+  };
+
+  const handleAddRepo = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
+
+    const response = await api.get<GithubRepository>(`/repos/${newRepo}`);
+    const repository = response.data;
+    setRepos([...repos, repository]);
+    setNewRepo('');
+  };
+
   return (
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do GitHub</Title>
-      <Form>
-        <input type="text" placeholder="username/repository_name" />
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+        <input
+          type="text"
+          placeholder="username/repository_name"
+          onChange={handleInputChange}
+        />
         <button type="submit">Buscar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repos>
-        <a href="/repositories">
-          <img
-            src="https://avatars.githubusercontent.com/u/68967867?v=4"
-            alt="Repositório"
-          />
-          <div>
-            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
-            <p>Repositório do mini curso gratuito de reactjs</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repos.map(repository => (
+          <a href="/repositories" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repos>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -18,13 +22,24 @@ export const Form = styled.form`
     flex: 1;
     height: 70px;
     padding: 0 24px;
-    border: 2px solid #fff;
+    border: none;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
-    border-right: 0;
+    outline: none;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border: 1px solid #c53030;
+        border-right: none;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
+    }
+
+    &:focus {
+      box-shadow: inset 0 0 3px 1px #bbb;
     }
   }
 
@@ -89,4 +104,10 @@ export const Repos = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
